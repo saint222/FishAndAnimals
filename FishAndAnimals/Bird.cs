c5987ff62e0ebc40f767261529448a907003fc84
saint12maloj <saint12maloj@gmail.com>
2018-12-20 21:59:20 +0300

    Классы с Enums готовы

---
+++ FishAndAnimals/Bird.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishAndAnimals
+{
+    public class Bird : Animal
+    {
+        public Colour Colour { get; set; }
+    }
+}
---
+++ FishAndAnimals/Colour.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishAndAnimals
+{
+    public enum Colour
+    {
+        red,
+        blue,
+        black,
+        white,
+        mixed
+    }
+}
